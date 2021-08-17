c23eca800d6c58e431954f19728a4fba7edd9914
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 11:49:50 -0400

    Added ability to set and see RNG state

---
+++ Patches/OnScreenDebugInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using InControl;
+using Language;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class patch_OnScreenDebugInfo : OnScreenDebugInfo 
+{
+
+}
